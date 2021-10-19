480b11f945198fd6d7891b59188235c5db2b3e8c
Javier <coderdiaz@gmail.com>
2021-10-18 20:03:21 -0500

    feat: added support to connect mongo db

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Connect to MongoDB
+const connectMongoDB = () => mongoose.connect('mongodb://localhost/babali', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get('/', (req, res) => {
   return res.json({
@@ -10,6 +17,8 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log(`Babali Shop API on port ${3000}`);
+connectMongoDB().then(() => {
+  app.listen(3000, () => {
+    console.log(`Babali Shop API on port ${3000}`);
+  })
 });
