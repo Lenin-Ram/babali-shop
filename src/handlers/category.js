cf3e8ad98395dc7b3974d565996585c2c73bceba
Javier <coderdiaz@gmail.com>
2021-10-18 21:01:34 -0500

    feat: added category routers

--- src/handlers/category.js
+++ src/handlers/category.js
@@ -2,6 +2,31 @@ const express = require('express');
 const router = express.Router();
 const { CategoryModel: Category } = require('../models/category');
 
+// Find all categories
+router.get('/', async (req, res) => {
+  const categories = await Category.find().exec();
+
+  return res.json({
+    results: categories,
+  });
+});
+
+// Find category by id
+router.get('/:id', async (req,res) => {
+  const { id } = req.params;
+  const category = await Category.findById(id);
+
+  if (!category) {
+    return res.status(404).json({
+      message: 'Category not found',
+    });
+  }
+
+  return res.json({
+    data: category,
+  });
+});
+
 // Create a new category
 router.post('/', async (req, res) => {
   const {
@@ -20,6 +45,42 @@ router.post('/', async (req, res) => {
 
   await category.save();
   return res.status(201).json();
+});
+
+// Delete category by id
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  await Category.findByIdAndDelete(id);
+
+  return res.json({
+    message: 'Category deleted',
+  });
+});
+
+// Modify category by id
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const {
+    name,
+    description,
+    featured_image,
+  } = req.body;
+
+  const category = await Category.findByIdAndUpdate(id, {
+    name,
+    description,
+    featured_image,
+  }, { new: true })
+
+  if (!category) {
+    return res.status(404).json({
+      message: 'Category not found',
+    });
+  }
+
+  return res.json({
+    data: category,
+  });
 });
 
 module.exports = router;
